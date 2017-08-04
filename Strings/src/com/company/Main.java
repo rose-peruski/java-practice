bbc7e6b9857bec10cd24aeb0307ec33a522e6de6
Rose Peruski <rose.peruski@gmail.com>
2017-08-04 14:15:26 -0400

    working on string unit

--- Strings/src/com/company/Main.java
+++ Strings/src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	String myString = "Jasper is adorable";
+	int myInt = 50;
+	String myNum = "50";
+        System.out.println("What do I think of my dog? " + myString);
+        System.out.println("Adding a string and int: " + myNum + myInt );
     }
 }
